d1ab6f5fe9c148ee4c56f681648ceb69bd23c854
YohnSuh <yohnsuh@gmail.com>
2024-05-17 21:18:49 +0900

    d-3 제작 중

--- js/d-3.js
+++ js/d-3.js
@@ -1,4 +1,19 @@
-let conWidth = $('.contents').width();
-$('nav .sub').css({
-    width: conWidth
+let mw = $('.contents').width();
+$('nav > ul > li').mouseover(function() {
+    $('.menu-bg').stop().animate({width : mw},700);
+    $('.sub').stop().fadeIn();
+    $('.sub').css('display', 'flex');
 });
+$('nav > ul > li').mouseout(function() {
+    $('.menu-bg').stop().animate({width : 0},700);
+    $('.sub').stop().fadeOut();
+});
+
+$('.slide > div:gt(0)').hide();
+setInterval(function() {
+    $('.slide > div:first').fadeOut(1000)
+                           .next()
+                           .fadeIn(1000)
+                           .end()
+                           .appendTo('.slide');
+}, 3000);
