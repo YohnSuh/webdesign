fb5654add7aaab76643cda73a1c89d76626b1cd8
YohnSuh <yohnsuh@gmail.com>
2024-05-27 19:41:13 +0900

    팝업, 푸터 만들기

--- js/d-1.js
+++ js/d-1.js
@@ -4,3 +4,23 @@ $('nav > ul > li').mouseenter(function() {
 $('nav > ul > li').mouseleave(function() {
     $(this).find('.sub').stop().slideUp();
 });
+
+let d = 0;
+setInterval(function() {
+    if (d < 2) {
+        d++;
+    } else {
+        d = 0;
+    }
+    $('.slide > div').animate({top: d * -100 + '%'}, 1000);
+}, 3000);
+
+$('.tab > ul > li > a').click(function() {
+    $(this).addClass('on');
+    $(this).siblings().show();
+
+    $(this).parent().siblings().find('a').removeClass('on');
+    $(this).parent().siblings().find('div').hide();
+
+    return false;
+});
